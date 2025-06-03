5e4ff5d6427c514bbcdd2cbd81e2e613681a46f3
BABERMANN <cauahabermann7@gmail.com>
2025-06-02 22:08:07 -0300

    feat: add headers

--- Hello.php
+++ Hello.php
@@ -1,5 +1,8 @@
 <?php
+//ato 1 preparo dos bastidores
+//api fala para o cliente sobre respostas e permissoes
 
-echo "Olá, Mundo!";
+header("Access-Control-Allow-Origin: *");//permite que qualquer site acesse esta api
+header("Content-Type: application/json; charset=UTF-8");//
 
 ?>
